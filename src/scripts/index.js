9348a6af990e2997718bdcec08b78bda046018af
Febrianto <febrianto.bekasi@gmail.com>
2022-08-22 17:07:32 +0700

    add alert message and .prettierrc file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,75 +1,73 @@
 /* eslint-disable no-self-assign */
-import 'regenerator-runtime'; /* for async await transpile */
-import '../styles/main.scss';
-import '../styles/_responsive.scss';
-import 'bootstrap';
-import '@iconify/iconify';
-import './components/skip-to-content';
-import './components/navbar-app';
-import './components/footer-app';
-import './components/loading-animation';
-import Swal from 'sweetalert2';
-import 'lazysizes';
-import 'lazysizes/plugins/parent-fit/ls.parent-fit';
-import swRegister from './utils/sw-register';
-import App from './views/app';
+import "regenerator-runtime"; /* for async await transpile */
+import "../styles/main.scss";
+import "../styles/_responsive.scss";
+import "bootstrap";
+import "@iconify/iconify";
+import "./components/skip-to-content";
+import "./components/navbar-app";
+import "./components/footer-app";
+import "./components/loading-animation";
+import Swal from "sweetalert2";
+import "lazysizes";
+import "lazysizes/plugins/parent-fit/ls.parent-fit";
+import swRegister from "./utils/sw-register";
+import App from "./views/app";
 
-const skipBtn = document.querySelector('skip-to-content');
+const skipBtn = document.querySelector("skip-to-content");
 
 const addActiveNavLink = (currentLocation, navLinks) => {
   navLinks.forEach((navLink) => {
     if (navLink.href === currentLocation) {
-      navLink.classList.add('active');
+      navLink.classList.add("active");
     } else {
-      navLink.classList.remove('active');
+      navLink.classList.remove("active");
     }
   });
 };
 
 const app = new App({
-  content: document.querySelector('#main-content'),
+  content: document.querySelector("#main-content"),
 });
 
 function stopVideo(modal) {
-  const currentIframe = modal.querySelector('.modal-body > iframe');
+  const currentIframe = modal.querySelector(".modal-body > iframe");
   currentIframe.src = currentIframe.src;
 }
 
-window.addEventListener('click', (event) => {
-  const {
-    target,
-  } = event;
-  if (target.classList.contains('modal')) {
-    target.style.display = 'none';
+window.addEventListener("click", (event) => {
+  const { target } = event;
+  if (target.classList.contains("modal")) {
+    target.style.display = "none";
     stopVideo(target);
   }
 });
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   app.renderPage();
   const currentLocation = window.location.href;
-  const navLinks = document.querySelectorAll('.nav-link');
+  const navLinks = document.querySelectorAll(".nav-link");
   addActiveNavLink(currentLocation, navLinks);
 });
 
-window.addEventListener('hashchange', () => {
+window.addEventListener("hashchange", () => {
   app.renderPage();
   swRegister();
   const currentLocation = window.location.href;
-  const navLinks = document.querySelectorAll('.nav-link');
+  const navLinks = document.querySelectorAll(".nav-link");
   addActiveNavLink(currentLocation, navLinks);
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-  skipBtn.addEventListener('click', () => {
-    document.querySelector('#main-content').focus();
+window.addEventListener("DOMContentLoaded", () => {
+  skipBtn.addEventListener("click", () => {
+    document.querySelector("#main-content").focus();
   });
 });
 
-window.addEventListener('offline', () => {
+window.addEventListener("offline", () => {
   Swal.fire(
-    'The Internet?',
-    'Oops, internet disconnected. check your connection.',
-    'question',
+    "The Internet?",
+    "Oops, internet disconnected. check your connection.",
+    "question"
   );
 });
--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,17 +1,17 @@
 import {
   createSearchBarTemplate,
   createStatisticContentTemplate,
-} from '../templates/template-creator';
+} from "../templates/template-creator";
 
-import CovidCasesIndoSource from '../../data/covid-cases-indo-source';
+import CovidCasesIndoSource from "../../data/covid-cases-indo-source";
 
 const Home = {
   async render() {
-    const navJumbo = document.querySelector('nav');
-    navJumbo.classList.add('withJumbo');
-    const mainContentElem = document.querySelector('#main-content');
-    if (mainContentElem.classList.contains('container')) {
-      mainContentElem.classList.remove('container');
+    const navJumbo = document.querySelector("nav");
+    navJumbo.classList.add("withJumbo");
+    const mainContentElem = document.querySelector("#main-content");
+    if (mainContentElem.classList.contains("container")) {
+      mainContentElem.classList.remove("container");
     }
 
     return `
@@ -21,25 +21,34 @@ const Home = {
   },
 
   async afterRender() {
-    document.title = 'Ranavid Apps';
-    const searchBar = document.querySelector('#search-bar');
+    document.title = "Ranavid Apps";
+    const searchBar = document.querySelector("#search-bar");
 
-    searchBar.innerHTML = createSearchBarTemplate('in-landing-page');
+    searchBar.innerHTML = createSearchBarTemplate("in-landing-page");
 
-    const statisticsBar = document.querySelector('#statistics');
+    const statisticsBar = document.querySelector("#statistics");
     const dataStatisticsCovidIndo = await CovidCasesIndoSource.totalCases();
 
-    statisticsBar.appendChild(createStatisticContentTemplate(dataStatisticsCovidIndo));
-    const ButtonsTypeOfHospitalization = document.querySelectorAll('button.form-check');
+    statisticsBar.appendChild(
+      createStatisticContentTemplate(dataStatisticsCovidIndo)
+    );
+    const ButtonsTypeOfHospitalization =
+      document.querySelectorAll("button.form-check");
 
     // for if btn element type of hospitalization on click, so tag input type radion is checked
     ButtonsTypeOfHospitalization.forEach((btnType) => {
-      btnType.addEventListener('click', (event) => {
-        if (event.target.classList.contains('form-check')) {
+      btnType.addEventListener("click", (event) => {
+        if (event.target.classList.contains("form-check")) {
           event.target.firstElementChild.checked = true;
         }
       });
     });
+
+    Swal.fire(
+      "Mohon maaf, untuk fitur pencarian ketersedian tempat tidur rumah sakit di aplikasi ini sedang tidak berfungsi",
+      "Tetapi anda dapat memakai fitur lainnya yang terdapat dalam aplikasi ini",
+      "question"
+    );
   },
 };
 
